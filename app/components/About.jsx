94cf8d4092bea20073ea2c25ae51c5acb2f6f905
Gornakoff <nikolaygornakov@yahoo.com>
2017-01-19 22:09:15 +0200

    add a paragraph to  About.jsx file

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@
 let About = React.createClass({
   render: function () {
     return (
-      <h3>About component!</h3>
+      <div>
+        <h3>About component!</h3>
+        <p>Welcome to the about page!</p>
+      </div>
     )
   }
 });
